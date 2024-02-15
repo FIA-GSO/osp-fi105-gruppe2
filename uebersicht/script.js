fce073850698a46e02ee332ab45624825fd73cea
benzn <ben.montag@web.de>
2024-02-15 08:20:00 +0100

    Files added from other repo
    added files from the other repo

---
+++ uebersicht/script.js
@@ -0,0 +1,18 @@
+function sayHello() {
+    console.log('Hello world!');
+}
+
+//Variables
+const name = null;
+const  status= null;
+const ausbilder = null;
+const date = null;
+
+class user{
+    constructor(name, status, ausbilder, date){
+        this._name = name;
+        this._status = status;
+        this._ausbilder = ausbilder;
+        this._date = date;
+    }
+}
