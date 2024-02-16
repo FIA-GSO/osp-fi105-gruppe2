62f8b2e5fcc1c40003360db2e5ab2873d625ce45
benzn <ben.montag@web.de>
2024-02-16 10:24:23 +0100

    change 16.2.24
    -

--- uebersicht/script.js
+++ uebersicht/script.js
@@ -1,18 +1,39 @@
-function sayHello() {
-    console.log('Hello world!');
-}
 
 //Variables
 const name = null;
-const  status= null;
+const status= null;
 const ausbilder = null;
 const date = null;
 
-class user{
-    constructor(name, status, ausbilder, date){
-        this._name = name;
-        this._status = status;
-        this._ausbilder = ausbilder;
-        this._date = date;
+// Test Variables
+const test_name = " Test Schüler";
+const  test_status= "OK";
+const test_ausbilder = "Test Ausbilder";
+const test_date = "99.99.9999";
+
+
+
+//add tablerow
+function  addTablerow() {
+    let table = document.getElementById("table");
+    let row = table.insertRow(table.rows.length);
+    row.insertCell(0).innerHTML = test_name;
+    row.insertCell(1).innerHTML = test_ausbilder;
+    row.insertCell(2).innerHTML = test_date;
+    row.insertCell(3).innerHTML = statusImage("OK");
+}
+
+function statusImage(status) {
+    switch (status) {
+        case "OK":
+            let pic = document.createElement("img");
+            pic.src = "files/correct.png";
+            return pic;
+        case "WAITING":
+            return "files/waiting.png";
+        case "WRONG":
+            return "files/wrong.png";
+        default:
+            return "OK";
     }
 }
