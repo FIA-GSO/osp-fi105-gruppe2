9537777463b2f06ffb1b454e02b166d58e0be3c7
Ben <68963882+thebenzon@users.noreply.github.com>
2024-02-16 14:55:47 +0100

    added javascript

--- uebersicht/script.js
+++ uebersicht/script.js
@@ -7,33 +7,47 @@ const date = null;
 
 // Test Variables
 const test_name = " Test Schüler";
-const  test_status= "OK";
+const  test_status= "ok";
+const  test_status_waiting= "waiting";
+const  test_status_wrong= "wrong";
 const test_ausbilder = "Test Ausbilder";
 const test_date = "99.99.9999";
 
 
 
 //add tablerow
-function  addTablerow() {
+function  addTablerow(name, ausbilder, date, status) {
     let table = document.getElementById("table");
     let row = table.insertRow(table.rows.length);
-    row.insertCell(0).innerHTML = test_name;
-    row.insertCell(1).innerHTML = test_ausbilder;
-    row.insertCell(2).innerHTML = test_date;
-    row.insertCell(3).innerHTML = statusImage("OK");
-}
 
+    row.insertCell(0).innerHTML = name.toString();
+    row.insertCell(1).innerHTML = ausbilder.toString();
+    row.insertCell(2).innerHTML = date.toString();
+    row.insertCell(3).setAttribute("id", "row_image" + checkForRowIncrement());
+    let image = document.getElementById("row_image" + checkForRowIncrement());
+    image.appendChild(statusImage(status));
+}
+// Creating Status Image
 function statusImage(status) {
     switch (status) {
-        case "OK":
+        case "ok":
             let pic = document.createElement("img");
             pic.src = "files/correct.png";
             return pic;
-        case "WAITING":
-            return "files/waiting.png";
-        case "WRONG":
-            return "files/wrong.png";
+        case "waiting":
+            let pic1 = document.createElement("img");
+            pic1.src = "files/waiting.png";
+            return pic1;
+        case "wrong":
+            let pic2 = document.createElement("img");
+            pic2.src = "files/wrong.png";
+            return pic2;
         default:
-            return "OK";
+            return "-";
     }
+}
+//Check for Row Number
+function checkForRowIncrement() {
+    let table = document.getElementById("table");
+    return table.rows.length
 }
