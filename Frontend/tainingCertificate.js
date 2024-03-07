4156bd2b49d924498e2e0a8dc0e05b6054095a99
Patrick Ortmann <patrick.o7@SCHULE.KOELN>
2024-03-07 10:53:27 +0100

    nachweise Json weiterarbeit

--- Frontend/tainingCertificate.js
+++ Frontend/tainingCertificate.js
@@ -2,7 +2,7 @@ function addContentRow(day) {
     //Add Row
     document.getElementById(day + 'AddRow')
                 .insertAdjacentHTML("beforebegin",
-                    `<tr class="tableContent">
+                    `<tr class="tableContent `+ day.toLowerCase() +`">
                         <td>
                             <textarea class="trainingActivities" type="text" placeholder="Hier ist deine Textbox"></textarea>
                         </td>
@@ -25,8 +25,113 @@ function addContentRow(day) {
     department.rowSpan = newDepartmentRowSpan;
 }
 
+function GetCertificateAsJson() {
 
-window.onerror = function(message, source, lineno, colno, error) {
-    console.error("Error:", message, "at", source, "line:", lineno, "column:", colno, error);
-    return true; // Prevent default error handling
-};
+    // var contentJson = {
+    //     monday: {
+    //       department: string
+    //     },
+    //     tuesday: {
+    //       department: string
+    //     },
+    //     wednesday: {
+    //       department: string
+    //     },
+    //     thursday: {
+    //       department: string
+    //     },
+    //     friday: {
+    //       department: string
+    //     },
+    //     saturday: {
+    //         department: string
+    //       },
+    //     requested: false
+    //   }
+
+    function FilterTableContent(element) {
+        return element.className.includes('tableContent')
+    }
+
+    function FilterActivities (element) {
+        return element.className.includes('activitieContent')
+    }
+
+    function SetDepartmentForDay (day) {
+        var xy = day;
+        switch(day){
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+            case "monday":
+            break;
+        }
+        
+    }
+
+    function SetDepartmentFor () {
+        
+    }
+
+    let tableBody = document.getElementById('trainingCertificateTable').children[0];
+    console.log(tableBody.className);
+
+    // Array für das Json
+    // var entrie = {
+    //     entries: [
+    //     {
+    //       workDone: string,
+    //       hours: 0
+    //     }
+    //   ],
+    // }
+
+
+    // Alle Rows mit Einträgen die obersten mit Gesamtstunden und Abteilung haben in der Klasse Top stehen
+    let trainingCertificateTableContents =  Array.from(tableBody.children).filter(FilterTableContent);
+
+    for(let i = 0; i <  trainingCertificateTableContents.length; i++) {
+        // let  entrie = {
+        //     entries: [
+        //     {
+        //       workDone: string,
+        //       hours: 0
+        //     }
+        //   ],
+        // }
+
+
+        if(trainingCertificateTableContents[i].className.includes("top"))
+        {
+            let department = trainingCertificateTableContents[i].children[4].children[0].value;
+            SetDepartmentForDay(trainingCertificateTableContents[i].className.replace("tableContent ", "").replace(" top", ""));
+        }
+        let activities = Array.from(trainingCertificateTableContents[i].children).filter(FilterActivities);
+
+
+        for(let i = 0; i < activities.length; i + 2){
+            let activitie = activities[i].nodeValue;
+			let xy = activitie.classname;
+            let timeForActivitie = activities[i + 1].nodeValue;
+			let yx = timeForActivitie.classname;
+        }
+    }
+
+    for (let i = 0; i < trainingCertificateTableContents.length; i++) {
+        console.log(trainingCertificateTableContents[i].className);
+        console.log(trainingCertificateTableContents[i]);
+      }
+}
