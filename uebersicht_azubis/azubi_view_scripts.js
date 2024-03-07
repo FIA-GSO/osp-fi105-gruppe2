e57148a11bb4affd5e14111d02cdfa1eaa7b005c
benzn <ben.montag@web.de>
2024-03-07 10:57:02 +0100

    some changes

--- uebersicht_azubis/azubi_view_scripts.js
+++ uebersicht_azubis/azubi_view_scripts.js
@@ -6,3 +6,13 @@ function  addTablerowForAzubiView(name, ausbilder) {
     row.insertCell(0).innerHTML = "<a  id='name' href='#'>" + name + "</a>";
     row.insertCell(1).innerHTML = ausbilder.toString();
 }
+
+function  handlerAbmeldenAnmelden(){
+    console.log("Abmelden")
+    return null
+
+}
+
+
+let abmeldenAnmelden = document.getElementById("abmeldenAnmeldenID")
+abmeldenAnmelden.addEventListener("click", handlerAbmeldenAnmelden());
