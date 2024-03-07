ca7c1578a941f0d0bc6f687b63fcc476d6112051
Patrick <113288566+PatrickOrtDev@users.noreply.github.com>
2024-03-07 13:19:05 +0100

    Update tainingCertificate.js mit Json ersteller

--- Frontend/tainingCertificate.js
+++ Frontend/tainingCertificate.js
@@ -25,113 +25,56 @@ function addContentRow(day) {
     department.rowSpan = newDepartmentRowSpan;
 }
 
-function GetCertificateAsJson() {
-
-    // var contentJson = {
-    //     monday: {
-    //       department: string
-    //     },
-    //     tuesday: {
-    //       department: string
-    //     },
-    //     wednesday: {
-    //       department: string
-    //     },
-    //     thursday: {
-    //       department: string
-    //     },
-    //     friday: {
-    //       department: string
-    //     },
-    //     saturday: {
-    //         department: string
-    //       },
-    //     requested: false
-    //   }
-
-    function FilterTableContent(element) {
-        return element.className.includes('tableContent')
+function GetCertificateAsJson(proofRequested) {
+    function GetEntries (day) {
+      
+        var content = document.getElementsByClassName("tableContent " + day.toLowerCase()); 
+        let entries = new Array();;
+        for(let i = 0; i < content.length; i++){
+            let workDone = content[i].children.length > 3 ? content[i].children[1].children[0].value : content[i].children[0].children[0].value;
+            let hours = content[i].children.length > 3 ? content[i].children[2].children[0].value :  content[i].children[1].children[0].value;
+                    entries.push(
+                        {
+                            workDone: workDone,
+                            hours: hours
+                        }
+                    );
+                }
+                return entries;
     }
 
-    function FilterActivities (element) {
-        return element.className.includes('activitieContent')
+    function GetDepartment (day) {
+        var department = document.getElementById("department" + day).children[0].value;
+        return department;
     }
 
-    function SetDepartmentForDay (day) {
-        var xy = day;
-        switch(day){
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-            case "monday":
-            break;
-        }
-        
-    }
-
-    function SetDepartmentFor () {
-        
-    }
-
-    let tableBody = document.getElementById('trainingCertificateTable').children[0];
-    console.log(tableBody.className);
-
-    // Array für das Json
-    // var entrie = {
-    //     entries: [
-    //     {
-    //       workDone: string,
-    //       hours: 0
-    //     }
-    //   ],
-    // }
-
-
-    // Alle Rows mit Einträgen die obersten mit Gesamtstunden und Abteilung haben in der Klasse Top stehen
-    let trainingCertificateTableContents =  Array.from(tableBody.children).filter(FilterTableContent);
-
-    for(let i = 0; i <  trainingCertificateTableContents.length; i++) {
-        // let  entrie = {
-        //     entries: [
-        //     {
-        //       workDone: string,
-        //       hours: 0
-        //     }
-        //   ],
-        // }
-
-
-        if(trainingCertificateTableContents[i].className.includes("top"))
-        {
-            let department = trainingCertificateTableContents[i].children[4].children[0].value;
-            SetDepartmentForDay(trainingCertificateTableContents[i].className.replace("tableContent ", "").replace(" top", ""));
-        }
-        let activities = Array.from(trainingCertificateTableContents[i].children).filter(FilterActivities);
-
-
-        for(let i = 0; i < activities.length; i + 2){
-            let activitie = activities[i].nodeValue;
-			let xy = activitie.classname;
-            let timeForActivitie = activities[i + 1].nodeValue;
-			let yx = timeForActivitie.classname;
-        }
-    }
-
-    for (let i = 0; i < trainingCertificateTableContents.length; i++) {
-        console.log(trainingCertificateTableContents[i].className);
-        console.log(trainingCertificateTableContents[i]);
+      var contentJson = {
+        monday: {
+            entries: GetEntries('Monday'),
+            department: GetDepartment('Monday')
+        },
+        tuesday: {
+            entries: GetEntries('Tuesday'),
+            department: GetDepartment('Tuesday')
+        },
+        wednesday: {
+            entries: GetEntries('Wednesday'),
+            department: GetDepartment('Wednesday')
+        },
+        thursday: {
+            entries: GetEntries('Thursday'),
+            department: GetDepartment('Thursday')
+        },
+        friday: {
+            entries: GetEntries('Friday'),
+            department: GetDepartment('Friday')
+        },
+        saturday: {
+            entries: GetEntries('Saturday'),
+            department: GetDepartment('Saturday')
+          },
+        requested: proofRequested
       }
+
+      return contentJson;
 }
